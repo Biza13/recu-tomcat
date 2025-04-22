6450d05d848c9db70c42565fe31c7233928ef2a5
Biza_13 <serrano_0_1@hotmail.com>
2025-04-22 19:49:04 +0200

    instalando docker compose

--- archivos-conf-script/ec2.sh
+++ archivos-conf-script/ec2.sh
@@ -16,3 +16,10 @@ sudo usermod -aG docker ec2-user
 
 # Verificación de los servicios
 sudo systemctl status docker --no-pager
+
+# Instalar Docker Compose
+DOCKER_COMPOSE_VERSION="1.29.2"  
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Establecer permisos de ejecución para docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
