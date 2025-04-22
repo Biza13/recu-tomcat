08470f7c14409e7c68b6a93f096d891fe8522bc8
Biza_13 <serrano_0_1@hotmail.com>
2025-04-22 19:28:38 +0200

    quitando provisioner ya que no lo voy a hacer en local

--- archivos-tf/s3-ec2.tf
+++ archivos-tf/s3-ec2.tf
@@ -68,18 +68,18 @@ resource "aws_instance" "instancia_fedora" {
 
   user_data = file("../archivos-conf-script/ec2.sh")
 
-  #Provisionador para copiar solo archivos
-  provisioner "file" {
-    source      = "../archivos-conf-script/Dockerfile"  # Ruta del directorio en tu máquina local
-    destination = "/home/ec2-user/Dockerfile"  # Ruta donde debe ir el directorio en la instancia
+  #Provisionador para copiar solo archivos !!!!solo funciona en local
+  #provisioner "file" {
+  #  source      = "../archivos-conf-script/Dockerfile"  # Ruta del directorio en tu máquina local
+  #  destination = "/home/ec2-user/Dockerfile"  # Ruta donde debe ir el directorio en la instancia
 
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"  # Asegúrate de que sea "ec2-user" para Amazon Linux
-      private_key = file("C:/Users/serra/.ssh/deployer-key.pem")  # Ruta a tu clave privada .pem
-      host        = self.public_ip  # Usa la IP pública de la instancia EC2
-    }
-  }
+    #connection {
+    #  type        = "ssh"
+    #  user        = "ec2-user"  # Asegúrate de que sea "ec2-user" para Amazon Linux
+    #  private_key = file("C:/Users/serra/.ssh/deployer-key.pem")  # Ruta a tu clave privada .pem
+    #  host        = self.public_ip  # Usa la IP pública de la instancia EC2
+    #}
+  #}
 }
 
 #bucket s3 para guardar tf state
