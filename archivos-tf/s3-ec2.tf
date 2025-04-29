de122ecbf80b12254444945251b4a7f5d970f5d4
Biza_13 <serrano_0_1@hotmail.com>
2025-04-29 19:04:05 +0200

    Modificacion grupo seguridad para pruebas

--- archivos-tf/s3-ec2.tf
+++ archivos-tf/s3-ec2.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "security" {
   }
 
   ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol="tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
