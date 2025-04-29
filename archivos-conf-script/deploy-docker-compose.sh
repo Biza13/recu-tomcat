f6bba3da9632f3308e14639262445b0170b2d50d
Biza_13 <serrano_0_1@hotmail.com>
2025-04-29 17:15:33 +0200

    Modificacion a revertir, solo para prueba

--- archivos-conf-script/deploy-docker-compose.sh
+++ archivos-conf-script/deploy-docker-compose.sh
@@ -28,8 +28,7 @@ docker compose version
 
 # Ejecutar docker compose
 echo "Ejecutando docker compose up..."
-cd /home/ec2-user/archivos-conf-script
-docker-compose up -d
+docker-compose -f ./archivos-conf-script/docker-compose.yml up -d
 
 echo "Proceso completado. Contenedores en ejecución:"
 docker ps
